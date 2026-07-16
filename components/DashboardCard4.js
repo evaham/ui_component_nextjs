750c0f4fa503d332bbc9261b825b5b86d913ed52
JungWoo Ham <evaham@naver.com>
2026-07-16 13:21:54 +0900

    style: LayoutShell 및 DashboardCard4 컴포넌트의 스타일 개선

--- components/DashboardCard4.js
+++ components/DashboardCard4.js
@@ -3,7 +3,6 @@
 export default function DashboardCard4({ type }) {
   return (
     <div className="p-6 rounded-2xl bg-white transition-all">
-
       {type === "sales" ? (
         <div className="flex items-center -mt-2 mb-2">
           <div className="flex items-center gap-2">
@@ -36,11 +35,11 @@ export default function DashboardCard4({ type }) {
       {type === "sales" ? (
       <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
         {/* 우리매장 */}
-        <div className="relative flex flex-col p-4 border border-zinc-100 rounded-2xl">
+        <div className="relative flex flex-col p-4 border border-zinc-200 rounded-2xl">
           <div className="flex items-center text-sm font-bold mb-2 tracking-tight">
             우리 매출액
           </div>
-          <div className="flex items-baseline gap-1 mb-1 font-bold text-blue-700">
+          <div className="flex items-baseline gap-1 mt-9 mb-1 font-bold text-blue-700">
             <div className="text-3xl tracking-tight">23,450,000</div>
             <span className="text-sm font-medium">원</span>
           </div>
@@ -52,16 +51,15 @@ export default function DashboardCard4({ type }) {
             <span className="font-bold">829</span>
           </div>
         </div>
-
         {/* 전국 200평 이하 */}
-        <div className="relative flex flex-col gap-1 p-4 border border-zinc-100 rounded-2xl">
+        <div className="relative flex flex-col gap-1 p-4 border border-zinc-200 rounded-2xl">
           <div className="flex gap-1 text-sm font-bold bg-zinc-100/50 px-3 py-1 rounded-full self-start">
-            <span>매장 순위</span>
+            <span>우리매장 순위</span>
             <span className="text-blue-600 font-bold">158</span>
             <span className="text-black/20">/</span>
             <span>1200</span>
           </div>
-          <div className="mt-2 text-sm font-medium tracking-tight">전국 200평 이하</div>
+          <div className="mt-2 text-sm font-medium tracking-tight">전국 200평 이하 평균</div>
           <div className="flex items-baseline gap-1">
             <div className="text-3xl font-bold tracking-tight">1,234,000,000</div>
             <span className="text-sm font-medium">원</span>
@@ -76,14 +74,14 @@ export default function DashboardCard4({ type }) {
         </div>
 
         {/* 인천 200평 이하 */}
-        <div className="relative flex flex-col gap-1 p-4 border border-zinc-100 rounded-2xl">
+        <div className="relative flex flex-col gap-1 p-4 border border-zinc-200 rounded-2xl">
           <div className="flex gap-1 text-sm font-bold bg-zinc-100/50 px-3 py-1 rounded-full self-start">
             <span>우리매장 순위</span>
             <span className="text-blue-600 font-bold">158</span>
             <span className="text-black/20">/</span>
             <span>1200</span>
           </div>
-          <div className="mt-2 text-sm font-medium tracking-tight">인천 200평 이하</div>
+          <div className="mt-2 text-sm font-medium tracking-tight">인천 200평 이하 평균</div>
           <div className="flex items-baseline gap-1">
             <div className="text-3xl font-bold tracking-tight">18,400,000</div>
             <span className="text-sm font-medium">원</span>
@@ -96,6 +94,7 @@ export default function DashboardCard4({ type }) {
             <span className="font-bold">829</span>
           </div>
         </div>
+
         {/* 전국평균 */}
         <div className="relative flex flex-col gap-1 p-4 bg-zinc-50 rounded-2xl">
           <div className="flex gap-1 text-sm font-bold bg-zinc-200 px-3 py-1 rounded-full self-start">
@@ -122,45 +121,47 @@ export default function DashboardCard4({ type }) {
       ) : type === "today" ? (
       <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
         {/* 우리매장 */}
-        <div className="relative flex flex-col px-4 py-3 bg-[#26499d] rounded-2xl">
-          <div className="text-sm font-bold text-zinc-100 mb-2">우리 매출액</div>
-          <div className="flex items-baseline gap-1 font-bold text-white">
+        <div className="relative flex flex-col p-4 border border-zinc-200 rounded-2xl">
+          <div className="flex items-center text-sm font-bold mb-2 tracking-tight">
+            우리 매출액
+          </div>
+          <div className="flex items-baseline gap-1 mb-1 font-bold text-blue-700">
             <div className="text-3xl tracking-tight">23,450,000</div>
             <span className="text-sm font-medium">원</span>
           </div>
-          <div className="flex gap-2 mt-2 px-3 py-1 text-sm font-bold text-white bg-black/30 rounded-full self-start">
-            <span className="font-medium">객수</span>
-            <span className="font-bold">829</span>
-            <span className="mx-1 text-white/50">/</span>
-            <span className="font-medium">객단가</span>
-            <span className="font-bold">829</span>
-          </div>
-        </div>
-
-        {/* 전국 200평 이하 */}
-        <div className="relative flex flex-col p-4 border border-zinc-100 rounded-2xl">
-          <div className="text-sm font-bold mb-2 tracking-tight">전국 200평 이하</div>
-          <div className="flex items-baseline gap-1 mb-1 font-bold">
-            <div className="text-3xl tracking-tight">1,234,000,000</div>
-            <span className="text-sm font-medium">원</span>
-          </div>
-          <div className="flex gap-1 mt-2 text-sm font-bold bg-zinc-100/50 px-3 py-1 rounded-full self-start">
+          <div className="flex gap-1 mt-2 text-sm text-blue-700 font-bold bg-zinc-100/50 px-3 py-1 rounded-full self-start">
             <span className="font-medium">객수</span>
             <span className="font-bold">829</span>
             <span className="mx-1 text-black/20">/</span>
             <span className="font-medium">객단가</span>
             <span className="font-bold">829</span>
           </div>
-          <div className="absolute top-3 right-3 flex gap-1 text-sm font-medium tracking-tight">
-            <span>순위</span>
+        </div>
+
+        {/* 전국 200평 이하 */}
+        <div className="relative flex flex-col gap-1 p-4 border border-zinc-200 rounded-2xl">
+          <div className="flex gap-1 text-sm font-bold bg-zinc-100/50 px-3 py-1 rounded-full self-start">
+            <span>매장 순위</span>
             <span className="text-blue-600 font-bold">158</span>
             <span className="text-black/20">/</span>
             <span>1200</span>
           </div>
+          <div className="mt-2 text-sm font-medium tracking-tight">전국 200평 이하</div>
+          <div className="flex items-baseline gap-1">
+            <div className="text-3xl font-bold tracking-tight">1,234,000,000</div>
+            <span className="text-sm font-medium">원</span>
+          </div>
+          <div className="flex gap-1 text-sm">
+            <span className="font-medium">객수</span>
+            <span className="font-bold">829</span>
+            <span className="mx-1 text-black/20">/</span>
+            <span className="font-medium">객단가</span>
+            <span className="font-bold">829</span>
+          </div>
         </div>
 
         {/* 인천 200평 이하 */}
-        <div className="relative flex flex-col p-4 border border-zinc-100 rounded-2xl">
+        <div className="relative flex flex-col p-4 border border-zinc-200 rounded-2xl">
           <div className="text-sm font-bold mb-2 tracking-tight">인천 200평 이하</div>
           <div className="flex items-baseline gap-1 mb-1 font-bold">
             <div className="text-3xl tracking-tight">18,400,000</div>
@@ -209,7 +210,7 @@ export default function DashboardCard4({ type }) {
       ) : type === "yesterday" ? (
       <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
         {/* 우리매장 */}
-        <div className="relative flex flex-col gap-1 p-4 border border-zinc-100 rounded-2xl">
+        <div className="relative flex flex-col gap-1 p-4 border border-zinc-200 rounded-2xl">
           <div className="flex items-center text-sm font-bold tracking-tight">
             우리 매출액
           </div>
@@ -233,7 +234,7 @@ export default function DashboardCard4({ type }) {
         </div>
 
         {/* 전국 200평 이하 */}
-        <div className="relative flex flex-col p-4 border border-zinc-100 rounded-2xl">
+        <div className="relative flex flex-col p-4 border border-zinc-200 rounded-2xl">
           <div className="flex items-center text-sm font-medium mb-2 tracking-tight">
             전국 200평 이하
           </div>
@@ -257,7 +258,7 @@ export default function DashboardCard4({ type }) {
         </div>
 
         {/* 인천 200평 이하 */}
-        <div className="relative flex flex-col p-4 border border-zinc-100 rounded-2xl">
+        <div className="relative flex flex-col p-4 border border-zinc-200 rounded-2xl">
           <div className="flex items-center text-sm font-medium mb-2 tracking-tight">
             인천 200평 이하
           </div>
@@ -322,7 +323,7 @@ export default function DashboardCard4({ type }) {
         </div>
 
         {/* 전국 200평 이하 */}
-        <div className="relative flex flex-col p-4 border border-zinc-100 rounded-2xl">
+        <div className="relative flex flex-col p-4 border border-zinc-200 rounded-2xl">
           <div className="text-sm font-bold mb-2">전국 200평 이하</div>
           <div className="flex items-baseline gap-1 mb-1 font-bold">
             <div className="text-3xl tracking-tight">1,234,000,000</div>
@@ -338,7 +339,7 @@ export default function DashboardCard4({ type }) {
         </div>
 
         {/* 인천 200평 이하 */}
-        <div className="relative flex flex-col p-4 border border-zinc-100 rounded-2xl">
+        <div className="relative flex flex-col p-4 border border-zinc-200 rounded-2xl">
           <div className="text-sm font-bold mb-2">인천 200평 이하</div>
           <div className="flex items-baseline gap-1 mb-1 font-bold">
             <div className="text-3xl tracking-tight">18,400,000</div>
